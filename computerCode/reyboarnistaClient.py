670669e8f6004b9fb304efa137037b00055a745f
Ricx8 <marquessricardo@gmail.com>
2020-08-10 20:59:06 -0400

    keyboard litener in the client side added

--- computerCode/reyboarnistaClient.py
+++ computerCode/reyboarnistaClient.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 
+from pynput.keyboard import Listener
+from pynput import keyboard
 import socket
+#import keyboard
+
+def onRelease(k):
+    key = str(k).replace('\'', "")
+
+    if k == keyboard.Key.esc:
+        return False
+    else:
+        if k == keyboard.Key.space:
+            key = ' '
+
+        if not 'Key.' in key:
+            s.send(key.encode())
+
 
 tcpIP = '192.168.1.11'
 tcpPort = 50005
@@ -10,10 +26,21 @@ msg = ""
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((tcpIP, tcpPort))
 
-while True:
-    msg = input("> ")
-    s.send(msg.encode())
+listener = Listener(on_release=onRelease)
+listener.start()
+listener.join()
 
-    if (msg == "exit"):
+'''while True:
+    key = keyboard.read_key()
+    if (key == "esc"):
+        print("EXIT")
         break
+    else:
+        if (key == "space"):
+            msg = " "
+        else:
+            msg = str(key)
+
+        s.send(msg.encode())'''
+
 s.close()
